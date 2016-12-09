8d804d3ebb999e8a2b0d889dcc4d6034fdeabca8
Zyzzz <zhouyan@imudges.com>
2016-12-09 21:03:11 +0800

    学生仓库

---
+++ src/main/java/com/imudges/repository/StudentRepository.java
@@ -0,0 +1,13 @@
+package com.imudges.repository;
+
+import com.imudges.model.StudentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by Administrator on 2016/12/9.
+ */
+@Repository
+public interface StudentRepository extends JpaRepository<StudentEntity,Integer> {
+    public StudentEntity findByEmail(String email);
+}
