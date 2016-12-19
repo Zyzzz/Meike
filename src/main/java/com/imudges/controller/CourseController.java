cb994167266719abca31e8579a24fef353c414c0
Zyzzz <zhouyan@imudges.com>
2016-12-19 15:55:31 +0800

    得到所以课程信息

--- src/main/java/com/imudges/controller/CourseController.java
+++ src/main/java/com/imudges/controller/CourseController.java
@@ -1,11 +1,17 @@
 package com.imudges.controller;
 
+import com.imudges.model.CourseEntity;
+import com.imudges.model.CourseInformationEntity;
+import com.imudges.model.CourseListEntity;
+import com.imudges.model.PictureEntity;
 import com.imudges.repository.CourseRepository;
+import com.imudges.repository.PictureRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,11 +21,28 @@ import java.util.List;
 public class CourseController {
     @Autowired
     CourseRepository courseRepository;
+    PictureRepository pictureRepository;
 
     @ResponseBody
     @RequestMapping(value = "/getAllCourse")
-    public List getAllCourse(){
-       return courseRepository.findAll();
+    public CourseListEntity getAllCourse(){
+        List<CourseEntity> courseEntities = courseRepository.findAll();
+        List<PictureEntity> pictureEntities  = pictureRepository.findByPattern(1);
+        CourseInformationEntity courseInformationEntity = new CourseInformationEntity();
+        List<CourseInformationEntity> courseInformationEntities = new ArrayList<>();
+        CourseListEntity courseListEntity = new CourseListEntity();
+        for(int i=0;i<courseEntities.size();i++){
+            for(int j=0;j<pictureEntities.size();j++){
+                if(courseEntities.get(i).getId()==pictureEntities.get(j).getOtherid()){
+                    courseInformationEntity.setCourseEntity(courseEntities.get(i));
+                    courseInformationEntity.setPictureEntity(pictureEntities.get(j));
+                    courseInformationEntities.add(courseInformationEntity);
+                }
+            }
+        }
+        courseListEntity.setStatus(0);
+        courseListEntity.setCourseInformationEntities(courseInformationEntities);
+        return courseListEntity;
     }
 
 }
--- src/main/java/com/imudges/repository/PictureRepository.java
+++ src/main/java/com/imudges/repository/PictureRepository.java
@@ -3,8 +3,11 @@ package com.imudges.repository;
 import com.imudges.model.PictureEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * Created by Administrator on 2016/12/19.
  */
 public interface PictureRepository  extends JpaRepository<PictureEntity,Integer> {
+    public List<PictureEntity> findByPattern(int pattern);
 }
