8d6919402bd3d56ff3c1a3d4cf28bba88ac5dc29
duola-shuang <1342741446@qq.com>
2017-01-14 16:53:57 +0800

    AdminRepository

--- src/main/java/com/imudges/controller/AdminController.java
+++ src/main/java/com/imudges/controller/AdminController.java
@@ -1,10 +1,13 @@
 package com.imudges.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 /**
  * Created by Administrator on 2017/1/14.
  */
 @Controller
-public class AdminContruller {
+public class AdminController {
+
+
 }
