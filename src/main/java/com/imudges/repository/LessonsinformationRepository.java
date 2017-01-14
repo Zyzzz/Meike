188339bc031c1d5101471613959472e05f5dcdf0
Zyzzz <zhouyan@imudges.com>
2017-01-14 14:36:28 +0800

    查找一个课程的信息

---
+++ src/main/java/com/imudges/repository/LessonsinformationRepository.java
@@ -0,0 +1,10 @@
+package com.imudges.repository;
+
+import com.imudges.model.LessonsinformationEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by Administrator on 2017/1/14.
+ */
+public interface LessonsinformationRepository  extends JpaRepository<LessonsinformationEntity,Integer> {
+}
