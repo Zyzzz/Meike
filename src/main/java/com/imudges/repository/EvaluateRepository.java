c25128ca819306184806b38268f0045023a80071
cyy <chenyangyang@imudges.com>
2017-01-15 13:54:56 +0800

    ..

---
+++ src/main/java/com/imudges/repository/EvaluateRepository.java
@@ -0,0 +1,13 @@
+package com.imudges.repository;
+
+import com.imudges.model.EvaluateEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by cyy on 2017/1/15.
+ */
+@Repository
+public interface EvaluateRepository extends JpaRepository<EvaluateEntity,Integer> {
+
+}
